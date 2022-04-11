ae24bd5c7447919e8ae9e515a89bf4b8134aa35b
Mujtaba Hassan <smhassan@knox.edu>
2022-04-11 00:26:22 -0500

    Removed bloatware bloc stuff

--- customer_service/lib/screens/chats_test/lib/data/validators/name_validator.dart
+++
@@ -1,5 +0,0 @@
-class NameFieldValidator {
-  static String validate(String value) {
-    return value.isEmpty ? 'Name cannot be empty' : null;
-  }
-}
--- customer_service/lib/screens/chats_test/lib/data/validators/password_validator.dart
+++
@@ -1,5 +0,0 @@
-class PasswordFieldValidator {
-  static String validate(String value) {
-    return value.isEmpty ? 'Password cannot be empty' : null;
-  }
-}
